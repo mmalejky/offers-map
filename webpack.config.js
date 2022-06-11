596a8c7060d19d0e36fcda75e371360d14737dd7
Marcin Malejky <m.malejky@student.uw.edu.pl>
2022-06-11 09:37:36 +0200

    Fixed wrong dist path

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: 'dist/',
     filename: 'bundle.js',
   },
   devServer: {
